cce8e934f106dd55e8dcb22f3944df2703a27bf5
Ro Ramtohul <yoitsro@hotmail.com>
2014-10-27 22:18:47 +0000

    Added logging

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var Hapi = require('hapi'); // Our server framework
 var RoutesLib = require('./routes'); // Our routes
 
 // Create the server
-var server = new Hapi.createServer(process.env.HOST || 'localhost', process.env.PORT || 9001, {cors: true});
+var server = new Hapi.createServer(process.env.HOST || 'localhost', process.env.PORT || 9001, {cors: true, debug: {request:['error']}});
 
 // Set up the route(s)
 server.route([
