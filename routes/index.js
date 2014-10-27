cf303dd3d602fb6d276465ddb68db558f85afa20
Ro Ramtohul <yoitsro@hotmail.com>
2014-10-27 22:32:50 +0000

    Added some comments

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,4 @@
 var Async = require('async'); // An async helper library
-var Hoek = require('hoek'); // A helper library
 var ScraperLib = require('../lib'); // Our functions to scrape the various parts of the site
 
 /**
@@ -8,10 +7,12 @@ var ScraperLib = require('../lib'); // Our functions to scrape the various parts
  * @param  {Hapy.Reply}   reply   Our reply object
  */
 module.exports.grabSomeHeadlines = function(request, reply) {
+	// Use the waterfall flow to pass values from one
+	// function to the next
 	Async.waterfall([
 		ScraperLib.downloadHomepage,
 		ScraperLib.parseCategories,
-		ScraperLib.downloadCategoryPages,
+		ScraperLib.downloadAndParseCategoryPages,
 	], function(err, results) {
 		reply(results);
 	});
